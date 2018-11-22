600f2ab5ab3a82829315f2eca84f65540bab8c06
MauroDiamantino <maurogabriel.54@gmail.com>
2018-11-22 20:34:12 -0300

    Implementations of classes Command, Reply & ASWEEPReply. Testbench for these classes.
    Signed-off-by: MauroDiamantino <maurogabriel.54@gmail.com>

--- SNDKInterface.h
+++ SNDKInterface.h
@@ -1,33 +1,37 @@
 //Libraries
+#include <iostream>
 #include <vector>
 #include <string>
+#include <boost/algorithm/string/case_conv.hpp>
+
+using namespace std;
+using namespace ios;
 
 class Command{
 private:
-	string gruop;
 	vector<string> fields;
 	string variable;
-	int argument;
+	string argument;
 public:
 	Command();
 	~Command();
-	void SetSpectranVariable(string, unsigned int);
+	void SetSpectranVariable(string, string);
 	void ToAuthenticate(void);
 	void ToServer(string);
 	void GetInfoSpectran(string);
 	void SetUpSpectranFcn(string);
 	string GetString(void);
-	string GetGroup(){	return group;	}
+	string GetGroup(){	return fields[0];	}
 	string GetField(unsigned int);
 	string GetVariable(){	return variable;	}
-	int GetArgument(){	return argument;	}
-}
+	string GetArgument(){	return argument;	}
+	unsigned int GetNumOfFields(){	return fields.size();	}
+};
 
 class Reply{
 private:
-	string type;
 	vector<string> fields;
-	vector<string> subfields; //contien los subcampos de uno solo de los campos de la respuesta
+	vector<string> subfields; //contiene los subcampos de uno solo de los campos de la respuesta
 	char subfieldSeparator;
 public:
 	Reply();
@@ -35,19 +39,24 @@ public:
 	~Reply();
 	void SetUpReply(string);
 	string GetString();
-	string GetType(){	retunr type;	}
+	string GetType(){	return fields[0];	}
 	string GetField(unsigned int);
 	string GetSubfield(unsigned int);
-}
+	unsigned int GetNumOfFields(){	return fields.size();	}
+	unsigned int GetNumOfSubFields(){	return subfields.size();	}
+};
 
 class ASWEEPReply : public Reply{
 private:
-	vector<string> sweepValues; //corresponde a un solo subcampo
+	vector<string> powerValues; //corresponde a un solo subcampo
+	vector<string> freqValues;
 	char valueSeparator;
 public:
 	ASWEEPReply();
 	ASWEEPReply(string);
 	~ASWEEPReply();
-	void SetUpReply(string);
-	vector<string> GetSweepValues();
+	virtual void SetUpReply(string);
+	vector<string> GetPowerValues(){	return powerValues;	}
+	vector<string> GetFreqValues(){	return freqValues;	}
+	unsigned int GetNumOfSweepValues(){	return powerValues.size();	}
 };
